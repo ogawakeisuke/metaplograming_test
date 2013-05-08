492abef10d8e4b4cf8205a88bd73811fbb4e545f
ogawakeisuke <ogawa@uniba.jp>
2013-05-08 15:23:49 +0900

    リファクタリング

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,51 +1,24 @@
 class Track < ActiveRecord::Base
   attr_accessible :display_artist_name_en, :display_artist_name_jp, :display_mix_title_en, :display_mix_title_jp, :display_remixer_name_en, :display_remixer_name_jp, :display_title_en, :display_title_jp
-
-  #i18nでのカラム出し分けメソッド
-  def display_title(locale)
-    locale = locale.to_sym
-    case locale
-    when :ja then
-      self.display_title_jp
-    when :en then
-      self.display_title_com
-    else
-      self.display_title_com # default
-    end
+  
+  #rails流initializeらしい
+  after_initialize :init
+  def init
+    self.attribute_names.grep(/display_(.*)_/){Track.define_display_method $1}
   end
 
-  def display_mix_title(locale)
-    case locale
-    when :ja then
-      self.display_mix_title_jp
-    when :en then
-      self.display_mix_title_com
-    else
-      self.display_mix_title_com # default
+  def self.define_display_method(method_name)
+    define_method "display_#{method_name}" do |locale|
+      locale = locale.to_sym    
+      case locale
+      when :ja then
+        self.send("display_#{method_name}_jp")
+      when :en then
+        self.send("display_#{method_name}_com")
+      else
+        self.send("display_#{method_name}_com") # default
+      end 
     end
   end
-
-  def display_artist_name(locale)
-    case locale
-    when :ja then
-      self.display_artist_name_jp
-    when :en then
-      self.display_artist_name_com
-    else
-      self.display_artist_name_com # default
-    end
-  end
-
-  def display_remixer_name(locale)
-    case locale
-    when :ja then
-      self.display_remixer_name_jp
-    when :en then
-      self.display_remixer_name_com
-    else
-      self.display_remixer_name_com # default
-    end
-  end
-
 
 end
